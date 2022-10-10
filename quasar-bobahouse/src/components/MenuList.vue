9674cc97cd25f11485b534efbd3111328caa4024
Anuwat Bunsong <88655681+AnuwatBunsong@users.noreply.github.com>
2022-10-10 21:09:22 +0700

    Update Add On menu

--- quasar-bobahouse/src/components/MenuList.vue
+++ quasar-bobahouse/src/components/MenuList.vue
@@ -7,7 +7,7 @@
 
       <q-tabs v-model="tab" class="text-red-4">
         <q-tab label="Menu" name="menu" />
-        <q-tab label="Topping" name="topping" />
+        <q-tab label="Add On" name="addOn" />
       </q-tabs>
 
       <q-separator />
@@ -139,10 +139,129 @@
             </q-table>
           </q-tab-panel>
 
-          <q-tab-panel name="topping">
-            With so much content to display at once, and often so little screen
-            real-estate, Cards have fast become the design pattern of choice for
-            many companies, including the likes of Google and Twitter.
+          <q-tab-panel name="addOn">
+            <q-card-section class="flex flex-center">
+              <!-- Sample -->
+              <div class="q-pa-md q-gutter-sm">
+                <q-btn
+                  label="Add"
+                  color="white"
+                  text-color="dark"
+                  @click="addMenu = true"
+                />
+              </div>
+            </q-card-section>
+            <!-- Add menu -->
+            <q-card-section>
+              <q-dialog v-model="addMenu">
+                <q-card class="my-card" style="width: 400px">
+                  <q-img src="MilkTea.png" />
+
+                  <q-card-section>
+                    <q-form
+                      @submit="onSubmit"
+                      @reset="onReset"
+                      class="q-gutter-md"
+                    >
+                      <q-input
+                        filled
+                        type="text"
+                        v-model="topic"
+                        label="Add New Menu Name"
+                        lazy-rules
+                        :rules="[
+                          (val) =>
+                            (val && val.length > 0) || 'Please type Menu Name',
+                        ]"
+                      />
+
+                      <q-input
+                        filled
+                        type="text"
+                        v-model="price"
+                        label="Add Price"
+                        lazy-rules
+                        :rules="[
+                          (val) =>
+                            (val && val.length > 0) || 'Please give price',
+                        ]"
+                      />
+
+                      <div>
+                        <q-btn label="Submit" type="submit" color="primary" />
+                        <q-btn
+                          label="Reset"
+                          type="reset"
+                          color="primary"
+                          flat
+                          class="q-ml-sm"
+                        />
+                      </div>
+                    </q-form>
+                  </q-card-section>
+                </q-card>
+              </q-dialog>
+            </q-card-section>
+
+            <q-table
+              grid
+              grid-header
+              hide-header
+              title="All Menu"
+              :rows="rowsAddOn"
+              :columns="columnsAddOn"
+              row-key="name"
+              :filter="filter"
+              v-model:selected="selected"
+              selection="single"
+            >
+              <template v-slot:top-right>
+                <q-input
+                  borderless
+                  dense
+                  debounce="300"
+                  v-model="filter"
+                  placeholder="Search"
+                >
+                  <template v-slot:append>
+                    <q-icon name="search" />
+                  </template>
+                </q-input>
+              </template>
+
+              <template v-slot:item="props">
+                <div
+                  class="q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
+                  :style="props.selected ? 'transform: scale(0.95);' : ''"
+                >
+                  <q-card :class="props.selected ? 'bg-grey-2' : ''">
+                    <q-card-section>
+                      <q-checkbox
+                        dense
+                        v-model="props.selected"
+                        :label="props.row.name"
+                      />
+                    </q-card-section>
+                    <q-separator />
+                    <q-list dense>
+                      <q-item
+                        v-for="col in props.cols.filter(
+                          (col) => col.name !== 'topic'
+                        )"
+                        :key="col.name"
+                      >
+                        <q-item-section>
+                          <q-item-label>{{ col.label }}</q-item-label>
+                        </q-item-section>
+                        <q-item-section side>
+                          <q-item-label caption>{{ col.value }}</q-item-label>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-card>
+                </div>
+              </template>
+            </q-table>
           </q-tab-panel>
         </q-tab-panels>
       </div>
@@ -159,6 +278,7 @@ export default {
   setup() {
     const database = useAddMenu();
     const menuName = ref(null);
+    const topic = ref(null);
     const price = ref(null);
     submitNewMenu: ref(false);
 
@@ -213,7 +333,46 @@ export default {
         size: "S,M,L,",
       },
     ];
+    const columnsAddOn = [
+      {
+        name: "topic",
+        required: true,
+        label: "Topic",
+        align: "left",
+        field: (row) => rowsAddOn,
+        sortable: true,
+      },
+      {
+        name: "price",
+        align: "center",
+        label: "Price (Bath)",
+        field: "price",
+        sortable: true,
+      },
+      { name: "size", label: "Size", field: "size", sortable: true },
+    ];
 
+    const rowsAddOn = [
+      {
+        name: "Size",
+        size: "S,M,L,",
+      },
+      {
+        topic: "Sugar Level",
+        price: "95,115,145",
+        size: "S,M,L,",
+      },
+      {
+        topic: "Topping",
+        price: "95,115,145",
+        size: "S,M,L,",
+      },
+      {
+        topic: "Ice Level",
+        price: "95,115,145",
+        size: "S,M,L,",
+      },
+    ];
     return {
       tab: ref("menu"),
       filter: ref(""),
@@ -222,10 +381,12 @@ export default {
 
       columns,
       rows,
+      columnsAddOn,
+      rowsAddOn,
       menuName,
       price,
       database,
-
+      topic,
       onSubmit() {
         submitNewMenu: ref(true);
       },
